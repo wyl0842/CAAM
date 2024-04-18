7adb92d8e77de74044ab4b2cd1f8b301e49804ef
yl w <694776869@qq.com>
2024-04-18 18:25:19 +0800

    first commit

--- Co-teaching/example.sh
+++
@@ -1,1 +0,0 @@
-python main.py --dataset mnist --noise_type pairflip --noise_rate 0.45
--- Co-training-based_noisy-label-learning/datasets/__init__.py
+++
@@ -1,4 +0,0 @@
-from .noise_datasets import cifar_dataloader
-
-__all__ = ('cifar10_dataloader',
-           'ImbalanceCIFAR10', 'ImbalanceCIFAR100')
